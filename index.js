379b9f7437c00e12caee3b297c0157b42587a7de
tvqphuoc01 <tvqphuoc.01@gmail.com>
2021-06-01 21:16:16 +0700

    add New Pug File

--- index.js
+++ index.js
@@ -12,6 +12,26 @@ app.get('/', (req, res) => {
   res.render('index');
 })
 
+app.get('/service', (req, res) => {
+  res.render('service');
+})
+
+app.get('/aboutUs', (req, res) => {
+  res.render('aboutUs');
+})
+
+app.get('/logIn', (req, res) => {
+  res.render('logIn');
+})
+
+app.get('/menu', (req, res) => {
+  res.render('menu');
+})
+
+app.get('/signUp', (req, res) => {
+  res.render('signUp');
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
