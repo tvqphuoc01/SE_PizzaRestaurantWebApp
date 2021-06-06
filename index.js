a6a0a7c83515276402b7627cdb6768f9e908c21d
HMy2912 <danghoanmy.2912@gmail.com>
2021-06-06 22:28:11 +0700

    Contact page
    Sửa cái li.navbar để link vào page Contact, có sửa index.js

--- index.js
+++ index.js
@@ -1,41 +1,45 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.set('views', './views');
-app.set('view engine', 'pug');
+app.set("views", "./views");
+app.set("view engine", "pug");
 
-app.use(express.static('public/images'));
-app.use(express.static('public/style'));
+app.use(express.static("public/images"));
+app.use(express.static("public/style"));
 
-app.get('/', (req, res) => {
-  res.render('index');
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.get('/service', (req, res) => {
-  res.render('service');
-})
+app.get("/service", (req, res) => {
+  res.render("service");
+});
 
-app.get('/aboutUs', (req, res) => {
-  res.render('aboutUs');
-})
+app.get("/aboutUs", (req, res) => {
+  res.render("aboutUs");
+});
 
-app.get('/logIn', (req, res) => {
-  res.render('logIn');
-})
+app.get("/logIn", (req, res) => {
+  res.render("logIn");
+});
 
-app.get('/menu', (req, res) => {
-  res.render('menu');
-})
+app.get("/menu", (req, res) => {
+  res.render("menu");
+});
 
-app.get('/signUp', (req, res) => {
-  res.render('signUp');
-})
+app.get("/signUp", (req, res) => {
+  res.render("signUp");
+});
 
-app.get('/reservation', (req, res) => {
-  res.render('reservation');
-})
+app.get("/reservation", (req, res) => {
+  res.render("reservation");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
