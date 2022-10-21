10f24def7e023f5394c2e70b824e19f0aad3e4c9
tvqphuoc01 <57540319+tvqphuoc01@users.noreply.github.com>
2022-10-21 15:46:39 +0700

    Update staffLoginValidate.js

--- validate/staffLoginValidate.js
+++ validate/staffLoginValidate.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const firebase = require('../db');
-const Staff = require('../models/staff');
+const Staff = require('../models/Staff');
 const firestore = firebase.firestore();
 
 module.exports.validateLoginStaff = async function(req, res, next) {
